2a10d030b7da7299487b16d89ec923c2272a7794
Larry Weya <larryweya@gmail.com>
2012-07-31 16:31:14 +0300

    Added node_js spec to .travos.yml and fixed test to pass

--- spec/SampleSpecs.spec.js
+++ spec/SampleSpecs.spec.js
@@ -1,7 +1,7 @@
 describe('jasmine-node', function(){
 
   it('should pass', function(){
-    expect(1+2).toEqual(5);
+    expect(1+2).toEqual(3);
   });
 
   it('shows asynchronous test', function(){
